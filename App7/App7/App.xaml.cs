4dce76d97f0349617d97691824db03e5ffcc1051
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-07-06 19:12:49 +0200

    first commit

---
+++ App7/App7/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var navPage = new NavigationPage(new MainPage());
+            navPage.BarBackgroundColor = Color.FromHex("ffff00");
+
+            MainPage = navPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
